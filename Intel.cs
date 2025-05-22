f8dd4e41344549ce412e16fef7527807c3f4e7eb
Yakir Uzan <yakiruzangreen@gmail.com>
2025-05-22 15:15:59 +0300

    created intel class

--- Intel.cs
+++ Intel.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Data.Common;
+
+public class Intel
+{
+    public Terorrist Terorrist;
+    public string Location;
+    public string TimeStamp;
+
+    public Intel(Terorrist terorrist, string location, string timeStamp)
+    {
+        this.Terorrist = terorrist;
+        this.Location = location;
+        this.TimeStamp = timeStamp;   
+    }
+        
+}
