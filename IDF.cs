db79673561b18e851088686df36e4e0b784f7cce
Yakir Uzan <yakiruzangreen@gmail.com>
2025-05-25 15:12:56 +0300

    update

--- IDF.cs
+++ IDF.cs
@@ -1,4 +1,18 @@
-﻿public class IDF
+﻿using System.Collections.Generic;
+
+public class IDF
 {
-    public  int CreationDate;
+    public int CreationDate;
+    public string Commander;
+
+    public IDF(int CreationDate = 1948, string Commander = "Eyal Zamir")
+    {
+        this.CreationDate = CreationDate;
+        this.Commander = Commander;
+    }
+
+    //רשימת נשקים
+    public List<Weapons> ListWeapons = new List<Weapons>();
+    //רשימת ידיעות
+    public List<Intel> ListIntel = new List<Intel>();
 }
