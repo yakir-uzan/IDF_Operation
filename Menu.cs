e9d0e3cf03427bc6e325f06922f1cd6380bd96f7
David Tiechman <d0534110171@gmail.com>
2025-05-25 14:41:50 +0300

    template menu

--- Menu.cs
+++ Menu.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace IDF_Operation
 {
-    internal class Menu
+    public class Menu
     {
-    }
+        public Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("pleas enter a menu");
+                string shose = Console.ReadLine();
+                switch (shose)
+                {
+//לבדוק האם קיים מחבל עם השם הזאת
+                    case "a":
+                        break;
+                        //להוסיף מיקום למחבל הנל
+                    case "b":
+                        break;
+                        //לבדוק האם אפשרי להפעיל תקיפה למחבל הנל
+                    case "c":
+                        break;
+                        //להפעיל תקיפה
+                    case "d":
+                        break;
+                        //לבדוק האם התקיפה אכן בוצעה
+                }
+            }
+        }        
+    } 
 }
